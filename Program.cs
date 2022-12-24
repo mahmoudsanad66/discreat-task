748bb60edf46036f99b6e48f21db43c22ca8463d
mahmoudsanad66 <121308120+mahmoudsanad66@users.noreply.github.com>
2022-12-24 14:55:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace the_PRIME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, i;
+
+            Console.Write("Enter number one: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter number two: ");
+            int num2 = int.Parse(Console.ReadLine());
+            bool prime = true;
+            for (i = num1; i <= num2; i++)
+            {
+
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                        prime = false;
+
+                }
+                if (prime == true && i != 1 && i != 0)
+                { Console.WriteLine(i); }
+                prime = true;
+            }
+        }
+    }
+}
