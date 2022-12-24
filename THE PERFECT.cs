b089bcbf4dcfb89e1dc373b57f06798126c13954
mahmoudsanad66 <121308120+mahmoudsanad66@users.noreply.github.com>
2022-12-24 14:01:52 +0200

    Add files via upload

---
+++ THE PERFECT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_number_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float i, j;
+            Console.Write("enter number one: ");
+            float num1 = int.Parse(Console.ReadLine());
+            Console.Write("enter number two: ");
+            float num2 = int.Parse(Console.ReadLine());
+
+            for (i = num1; i <= num2; i++)
+            {
+                float sum = 0;
+                for (j = 1; j <= (i / 2); ++j)
+                {
+                    if (i % j == 0)
+                    { sum = sum + j; }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(sum);
+                }
+            }
+            }
+    }
+}
